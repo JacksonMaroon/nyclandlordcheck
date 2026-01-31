f796f9e6458595b794883cd7f41833fab8c0eeb0
Jackson Maroon <124005183+JacksonMaroon@users.noreply.github.com>
2026-01-31 00:30:03 -0500

    Add explicit favicon links

---
+++ frontend/src/app/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="icon" href="/icon.svg" type="image/svg+xml" sizes="any" />
+      <link rel="icon" type="image/png" href="/icon-144.png" sizes="144x144" />
+      <link rel="icon" type="image/png" href="/icon-96.png" sizes="96x96" />
+      <link rel="icon" type="image/png" href="/icon-48.png" sizes="48x48" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      <link rel="icon" href="/favicon.ico" sizes="any" />
+    </>
+  );
+}
